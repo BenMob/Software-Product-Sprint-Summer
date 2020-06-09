07b5b6bbdc91f54e3d0e03770cd302e937cc8953
Ben <benjaminombeni3@gmail.com>
2020-06-08 22:40:01 -0700

    Adds a basic Servlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -16,6 +16,7 @@ package com.google.sps.servlets;
 
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,9 +25,27 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private String name;
+  private String comment;
+
+  @Override
+  public void init() throws ServletException {
+      name = "N/A";
+      comment = "N/A";
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    name = request.getParameter("author");
+    comment = request.getParameter("comment");
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("<h2>Name: " + name + "</h2>");
+    response.getWriter().println("<p>Comment: " + comment + " </p> <hr>");
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+      doGet(request, response);
   }
 }
