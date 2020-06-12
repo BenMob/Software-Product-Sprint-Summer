97339715546cc3f6d919127f958ba3d63e297bff
Ben <benjaminombeni3@gmail.com>
2020-06-11 20:42:34 -0700

    Json Demo

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -12,21 +12,56 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-/**
-    Fetch demo
+/*****************************************************************
+ * This function requests data from the server using async await
+ * @param url: The url to request from 
+ * @param containerId: the id of the conatiner in which this data is 
+ *                    to be diplayed on the user interface. 
  */
- function fetchData(){
-     fetch('/data')
-     .then((response) => response.text()
-     .then((text) => showData(text)), () => console.log("Failed to get informatin at /data"))
- }
-
- function showData(data){
-     let container = document.getElementById('data-container').classList
-     let content = document.getElementById('data')
-
-     container.add('w3-container', 'w3-border', 'w3-white')
-     content.innerText = data
-     
+ async function fetchDataAsync(url, containerId){
+     const response = await fetch(url)
+     const data = await response.json()
+     presentData(data, containerId)
+     console.log(data)
  }
  
+/****************************************************************
+ *This fcuntion processes reponses from the server appropriatelly
+ * and dpresents displays them on the page
+ * @param data: an array containing the data returned from the
+ *                 server 
+ *********/
+function presentData(data, containerId){
+    const container = document.getElementById(containerId)
+    let ul = document.createElement('ul');
+    addClass(ul, 'w3-ul', 'w3-round-16') // Adds some classes to the ul for styling
+    data.forEach(dataItem => {
+        ul.appendChild(createHtmlTag('li', dataItem))     
+    })
+    container.appendChild(ul)
+}
+
+/***************************************************************
+ *This function creates and html tag and populates it with text
+ * @param tagName: Any html tag name 
+ * @param text: text content you want to see in the tag
+ *********/
+ function createHtmlTag(tagName, text){
+     const tag = document.createElement(tagName)
+     const content = document.createTextNode(text)
+     tag.appendChild(content)
+     addClass(tag, 'w3-border-black') // Adds a class to the li for styling
+     return tag
+ }
+
+/***************************************************************
+ * This funtion adds a classes insite 
+ * @param htmlTag: Any HTML tag or element 
+ * @param ...classes: list of classes you wnat to add
+ *********/
+function addClass(htmlTag, ...cssClasses){
+    cssClasses.forEach((cssClass) => {
+        htmlTag.classList.add(cssClass)
+    })
+    return htmlTag
+}
