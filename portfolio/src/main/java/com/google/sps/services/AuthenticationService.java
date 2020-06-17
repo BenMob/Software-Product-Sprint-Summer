53fc83dd34c89c8fa664ec110a96859d133f964c
Ben <benjaminombeni3@gmail.com>
2020-06-16 18:01:27 -0700

    adds class squeletons

---
+++ portfolio/src/main/java/com/google/sps/services/AuthenticationService.java
@@ -0,0 +1,25 @@
+package com.google.sps.services;
+
+
+public final class AuthenticationSevice extends Services{
+
+    public static boolean userIsLoggedIn(){
+        return false;
+    }
+
+    public static String getLoginURL(){
+        return "null";
+    }
+
+    public static String getLogoutURL(){
+        return "null";
+    }
+
+    public static String getEmail(){
+        return "null";
+    }
+
+    public static String getUserNickname(){
+        return "null";
+    }
+}
